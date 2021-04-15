a3f9d8e2b82c8fbb5b54acf182ab48b3332d4c33
Ömer Oktay Gültekin <MrKty>
2021-04-15 22:01:57 +0300

    ok

--- app/src/main/java/com/union/unionapp/MainActivity.java
+++ app/src/main/java/com/union/unionapp/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.unionapp;
+package com.union.unionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.union.unionapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
