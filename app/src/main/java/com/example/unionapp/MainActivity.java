01cfd56bf12329e1aca4bf5fa7a9f55fb6f061fa
Ömer Oktay Gültekin <MrKty>
2021-04-15 21:09:20 +0300

    First Commit

--- app/src/main/java/com/example/unionapp/MainActivity.java
+++ app/src/main/java/com/example/unionapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello Yunus");
+        System.out.println("Oke Yunus");
     }
 }
