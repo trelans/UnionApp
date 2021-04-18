31579455fc3e3410de68f1befa8abdcb02cf180f
Ömer Oktay Gültekin <MrKty>
2021-04-18 21:30:57 +0300

    tagler tanımlandı

--- app/src/main/java/com/union/unionapp/CreateAnAccountActivity.java
+++ app/src/main/java/com/union/unionapp/CreateAnAccountActivity.java
@@ -3,12 +3,23 @@ package com.union.unionapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class CreateAnAccountActivity extends AppCompatActivity {
+    // Variables
+    TextView tw_email;
+    TextView tw_password;
+    TextView tw_forgot_password;
+    CheckBox check_box_remember_me;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_an_account);
+        tw_email = findViewById(R.id.emailTextView);
+        tw_password = findViewById(R.id.passwordTextView);
+        tw_forgot_password = findViewById(R.id.forgot_pass_tw);
+        check_box_remember_me = findViewById(R.id.CheckBoxRemember);
     }
 }
--- app/src/main/java/com/union/unionapp/LoginActivity.java
+++ app/src/main/java/com/union/unionapp/LoginActivity.java
@@ -9,6 +9,12 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    // Variables
+    TextView tw_email;
+    TextView tw_password;
+    TextView tw_forgot_password;
+    CheckBox check_box_remember_me;
+
     public void login (View view) {
         // Button onClick
         Log.i("Butona basıldı" , " YESS");
@@ -22,18 +28,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Variables
-        TextView tw_email;
-        TextView tw_password;
-        TextView tw_forgot_password;
-        CheckBox check_box_remember_me;
-
-
         tw_email = findViewById(R.id.emailTextView);
         tw_password = findViewById(R.id.passwordTextView);
         tw_forgot_password = findViewById(R.id.forgot_pass_tw);
         check_box_remember_me = findViewById(R.id.CheckBoxRemember);
-
-
     }
 }
