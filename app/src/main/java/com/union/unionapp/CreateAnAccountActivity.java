00975ca1e7ee43771b6eb481ba92c6c1f86ffd2e
Ömer Oktay Gültekin <MrKty>
2021-04-18 21:08:57 +0300

    layoutlar düzeltildi

--- app/src/main/java/com/union/unionapp/CreateAnAccountActivity.java
+++ app/src/main/java/com/union/unionapp/CreateAnAccountActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CreateAnAccountView extends AppCompatActivity {
+public class CreateAnAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_an_account_view);
+        setContentView(R.layout.activity_create_an_account);
     }
 }
