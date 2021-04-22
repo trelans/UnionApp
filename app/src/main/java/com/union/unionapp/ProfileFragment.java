584aa348a8c7df3669a759395fa9619be965ed18
mertunlu <69141853+mertunlu@users.noreply.github.com>
2021-04-22 18:40:24 +0300

    user profiledaki calendar pop up

--- app/src/main/java/com/union/unionapp/ProfileFragment.java
+++ app/src/main/java/com/union/unionapp/ProfileFragment.java
@@ -2,13 +2,16 @@ package com.union.unionapp;
 
 
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -27,14 +30,19 @@ public class ProfileFragment extends Fragment {
     TextView achsTextView;
     ListView lastActsList;
     ListView achsListView;
-
+    ImageView openCalendar;
+    Dialog calendarDialog;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        calendarDialog = new Dialog(getActivity());
+        // Layoutu transparent yapıo
+        calendarDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
+        openCalendar = (ImageView) view.findViewById(R.id.openCalendar);
         lastActsTextView = (TextView) view.findViewById(R.id.lastActsTextView);
         achsTextView = (TextView) view.findViewById(R.id.achsTextView);
 
@@ -68,6 +76,15 @@ public class ProfileFragment extends Fragment {
                 }
             }
 
+        });
+
+        openCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog;
+                calendarDialog.setContentView(R.layout.custom_user_calendar_popup);
+                calendarDialog.show();
+            }
         });
 
         return view;
