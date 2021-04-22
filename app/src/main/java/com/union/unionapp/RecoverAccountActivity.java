32f80099efb9d43f6ed3e12b5936112d14f9177b
YUNUS <yerent2000@gmail.com>
2021-04-22 18:21:06 +0300

    profile

--- app/src/main/java/com/union/unionapp/RecoverAccountActivity.java
+++ app/src/main/java/com/union/unionapp/RecoverAccountActivity.java
@@ -11,8 +11,6 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.concurrent.TimeUnit;
-
 public class RecoverAccountActivity extends AppCompatActivity {
     // Variables
     TextView tw_email;
