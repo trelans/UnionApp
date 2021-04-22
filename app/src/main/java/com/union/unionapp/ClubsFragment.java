1cd28b61503a320a723bcb32f47ff473fd59144b
mertunlu <69141853+mertunlu@users.noreply.github.com>
2021-04-23 01:46:10 +0300

    Club Create Post'a Spinner eklendi + Stack Create Post tekrar düzeltildi.

--- app/src/main/java/com/union/unionapp/ClubsFragment.java
+++ app/src/main/java/com/union/unionapp/ClubsFragment.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,24 +18,33 @@ import androidx.fragment.app.Fragment;
 
 public class ClubsFragment extends Fragment {
 
+    Dialog clubDialog;
+    Spinner clubTagSpinner;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_clubs, container, false);
 
 
-        Dialog myDialog;
+
         ImageView createPost = (ImageView) view.findViewById(R.id.showPopUpCreate);
-        myDialog = new Dialog(getActivity());
+        clubDialog = new Dialog(getActivity());
         // Layoutu transparent yapıo
-        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        clubDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
         createPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Dialog dialog;
-                myDialog.setContentView(R.layout.custom_create_club_post);
-                myDialog.show();
+                clubDialog.setContentView(R.layout.custom_create_club_post);
+
+                clubTagSpinner = clubDialog.findViewById(R.id.tagSpinner);
+                ArrayAdapter<CharSequence> tagAdapter = ArrayAdapter.createFromResource(getActivity(),R.array.stack_tags, android.R.layout.simple_spinner_item);
+                tagAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                clubTagSpinner.setAdapter(tagAdapter);
+
+                clubDialog.show();
             }
         });
 
