d6b5e722e0ec807fb4bfc3e1a9924430086e3ebe
trelans <59960883+trelans@users.noreply.github.com>
2021-04-23 01:17:35 +0300

    create club

--- app/src/main/java/com/union/unionapp/ClubsFragment.java
+++ app/src/main/java/com/union/unionapp/ClubsFragment.java
@@ -2,10 +2,13 @@ package com.union.unionapp;
 
 
 
+import android.app.Dialog;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +19,26 @@ public class ClubsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_clubs, container,false);
+        View view = inflater.inflate(R.layout.fragment_clubs, container, false);
+
+
+        Dialog myDialog;
+        ImageView createPost = (ImageView) view.findViewById(R.id.showPopUpCreate);
+        myDialog = new Dialog(getActivity());
+        // Layoutu transparent yapıo
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        createPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog;
+                myDialog.setContentView(R.layout.custom_create_club_post);
+                myDialog.show();
+            }
+        });
+
+
+
+        return view;
     }
 }
