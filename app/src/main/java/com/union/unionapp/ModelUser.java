93f1b358ea46f2806f086eea16fd22efca03b507
mertunlu <69141853+mertunlu@users.noreply.github.com>
2021-04-23 18:02:19 +0300

    ModelUser propertyler eklendi + getter setter eklendi + constructor eklendi

--- app/src/main/java/com/union/unionapp/ModelUser.java
+++ app/src/main/java/com/union/unionapp/ModelUser.java
@@ -1,11 +1,49 @@
 package com.union.unionapp;
 
+import java.util.ArrayList;
+
 public class ModelUser {
-    String username, email, search, profileImage , ui;
 
-    public  ModelUser() {
+    String username, email, search, profileImage, ui, password, notifications;
+    int accountType;
+    ArrayList<String> tags;
+    ArrayList<Integer> achievements, lastActivities, upcomingActivities;
+    boolean isClosed;
 
-    }
+
+    public ArrayList<Integer> getAchievements() { return achievements; }
+
+    public ArrayList<Integer> getUpcomingActivities() { return upcomingActivities; }
+
+    public void setUpcomingActivities(ArrayList<Integer> upcomingActivities) { this.upcomingActivities = upcomingActivities; }
+
+    public boolean isClosed() { return isClosed; }
+
+    public void setClosed(boolean closed) { isClosed = closed; }
+
+    public String getNotifications() { return notifications; }
+
+    public void setNotifications(String notifications) { this.notifications = notifications; }
+
+    public ArrayList<Integer> getLastActivities() { return lastActivities; }
+
+    public void setLastActivities(ArrayList<Integer> lastActivities) { this.lastActivities = lastActivities; }
+
+    public void setAchievements(ArrayList<Integer> achievements) { this.achievements = achievements; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public ArrayList<String> getTags() { return tags; }
+
+    public void setTags(ArrayList<String> tags) { this.tags = tags; }
+
+    public int getAccountType() { return accountType; }
+
+    public void setAccountType(int accountType) { this.accountType = accountType; }
+
+    public  ModelUser() { }
 
     public String getUsername() {
         return username;
@@ -47,11 +85,21 @@ public class ModelUser {
         this.ui = ui;
     }
 
-    public  ModelUser(String username, String email , String search, String profileImage, String ui ) {
+    public  ModelUser(String username, String email , String search, String profileImage, String ui,
+                      String password, int accountType, ArrayList<String> tags, ArrayList<Integer> achievements,
+                      ArrayList<Integer> lastActivities, String notifications, boolean isClosed, ArrayList<Integer> upcomingActivities) {
         this.username = username;
         this.email = email;
         this.search = search;
         this.profileImage = profileImage;
         this.ui = ui;
+        this.password = password;
+        this.accountType = accountType;
+        this.tags = tags;
+        this.achievements = achievements;
+        this.lastActivities = lastActivities;
+        this.notifications = notifications;
+        this.isClosed = isClosed;
+        this.upcomingActivities = upcomingActivities;
     }
 }
