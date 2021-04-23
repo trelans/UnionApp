5ab1510612c4db8618d0b72e1bdb1d87452370ad
mertunlu <69141853+mertunlu@users.noreply.github.com>
2021-04-23 19:06:02 +0300

    ClubFragment'a filter eklendi

--- app/src/main/java/com/union/unionapp/ClubsFragment.java
+++ app/src/main/java/com/union/unionapp/ClubsFragment.java
@@ -27,7 +27,7 @@ public class ClubsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_clubs, container, false);
 
 
-
+        ImageView filterImageView = (ImageView) view.findViewById(R.id.showClubFilterPopup);
         ImageView createPost = (ImageView) view.findViewById(R.id.showPopUpCreate);
         clubDialog = new Dialog(getActivity());
         // Layoutu transparent yapıo
@@ -48,6 +48,19 @@ public class ClubsFragment extends Fragment {
             }
         });
 
+        filterImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clubDialog.setContentView(R.layout.custom_club_filter);
+
+                clubTagSpinner = clubDialog.findViewById(R.id.tagSpinner);
+                ArrayAdapter<CharSequence> tagAdapter = ArrayAdapter.createFromResource(getActivity(),R.array.buddy_tags, android.R.layout.simple_spinner_item);
+                tagAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                clubTagSpinner.setAdapter(tagAdapter);
+
+                clubDialog.show();
+            }
+        });
 
 
         return view;
