d4ee8206c413efae21e3ce9339c1cd34ae766897
YUNUS <yerent2000@gmail.com>
2021-04-26 00:45:07 +0300

    user profile da last activities ve achievements arası geçis tamamlandı

--- app/src/main/java/com/union/unionapp/CustomAdapterLastActsPU.java
+++ app/src/main/java/com/union/unionapp/CustomAdapterLastActsPU.java
@@ -11,18 +11,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 
-public class CustomAdapterAchievements extends ArrayAdapter<String> {
+public class CustomAdapterLastActsPU extends ArrayAdapter<String> {
 
     //Properties
     Context c;
-    String[] achs;
+    String[] lastActs;
     LayoutInflater inflater;
 
     //Constructors
-    public CustomAdapterAchievements(@NonNull Context context, String[] achs) {
-        super(context, R.layout.achs_list_layout, achs);
+    public CustomAdapterLastActsPU(@NonNull Context context, String[] achs) {
+        super(context, R.layout.last_acts_list_layout_pu, achs);
         this.c = context;
-        this.achs = achs;
+        this.lastActs = achs;
 
     }
 
@@ -33,14 +33,14 @@ public class CustomAdapterAchievements extends ArrayAdapter<String> {
 
         if ( convertView == null ){
             inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.achs_list_layout, null);
+            convertView = inflater.inflate(R.layout.last_acts_list_layout_pu, null);
         }
 
         //Initializations
-        TextView achsTexts = (TextView) convertView.findViewById(R.id.achsItem);
+        TextView lastActsTexts = (TextView) convertView.findViewById(R.id.lastActsItemPU);
 
         //Set Text
-        achsTexts.setText(achs[position]);
+        lastActsTexts.setText(lastActs[position]);
         //image varsa setImageResource();
 
         return convertView;
